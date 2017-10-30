ea24e09007aa6ffe095c3c6a4495f7cbedbea76d
alicia1984 <alicia28031@hotmail.com>
2017-10-30 18:49:41 -0300

    primer  comit  de la  creacion  de  mi prueba

---
+++ src/pruebajava1/pruebajava1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava1;
+
+/*******************************************************************************
+ *Alicia Villarroel
+ * @author LP 13
+ * Fecha:Octubre 26 de 20017
+ * Programa: Clase3A
+ * Responsabilidad: clase principal
+ *****************************************************************************/
+public class pruebajava1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

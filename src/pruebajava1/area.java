9b782a6a5b9be27fe6390f3e4f6357a9ec2e80bc
alicia1984 <alicia28031@hotmail.com>
2017-10-30 19:23:23 -0300

    operacion  basica  para  calculaR  SOLICITADO  EN LA PRUEBA

---
+++ src/pruebajava1/area.java
@@ -0,0 +1,33 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area;
+
+/*******************************************************************************
+ *Alicia Villarroel
+ * @author LP 13
+ * Fecha:Octubre 30 de 20017
+ * Programa: pruebajava1
+ * Responsabilidad: calcular  el area del  triangulo
+ *****************************************************************************/
+public class area {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+// TODO code application logic hereç
+        int numerobase;//Declaracion de variable tipo entero base
+      double numeroaltura;//Declaracion variable tipo double altura
+     
+      
+       numerobase=4;//Acabo de inicalizar la variable numero
+      numeroaltura=5;//inicializo la variable numero2
+       System.out.println ("Multiplicacion de numero y numero2: "+(numerobase*numeroaltura));
+       
+    }
+}
